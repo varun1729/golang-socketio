bd32c7784f39fa852dfe162ca1f807901622adff
DESKTOP-EUBERBU\eddyf <eddyf@mail.com>
2017-10-11 07:06:15 +0300

    Realisation of writing message

--- transport/polling.go
+++ transport/polling.go
@@ -14,7 +14,9 @@ type PollingTransportParams struct {
 }
 
 type PollingConnection struct {
-	transport *PollingTransport
+	transport           *PollingTransport
+	eventsIn            chan<- string
+	SubscriptionHandler func(w http.ResponseWriter, r *http.Request)
 }
 
 func (plc *PollingConnection) GetMessage() (message string, err error) {
@@ -23,7 +25,7 @@ func (plc *PollingConnection) GetMessage() (message string, err error) {
 }
 
 func (plc *PollingConnection) WriteMessage(message string) error {
-
+	plc <- message
 	return nil
 }
 
@@ -134,3 +136,94 @@ func GetDefaultPollingTransport() *PollingTransport {
 // 	tr.Headers = params.Headers
 // 	return tr
 // }
+
+// get web handler that has closure around sub chanel and clientTimeout channnel
+func getLongPollSubscriptionHandler(maxTimeoutSeconds int, subscriptionRequests chan clientSubscription,
+	clientTimeouts chan<- clientCategoryPair, loggingEnabled bool) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		timeout, err := strconv.Atoi(r.URL.Query().Get("timeout"))
+		if loggingEnabled {
+			log.Println("Handling HTTP request at ", r.URL)
+		}
+		// We are going to return json no matter what:
+		w.Header().Set("Content-Type", "application/json")
+		// Don't cache response:
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate") // HTTP 1.1.
+		w.Header().Set("Pragma", "no-cache")                                   // HTTP 1.0.
+		w.Header().Set("Expires", "0")                                         // Proxies.
+		if err != nil || timeout > maxTimeoutSeconds || timeout < 1 {
+			if loggingEnabled {
+				log.Printf("Error: Invalid timeout param.  Must be 1-%d. Got: %q.\n",
+					maxTimeoutSeconds, r.URL.Query().Get("timeout"))
+			}
+			io.WriteString(w, fmt.Sprintf("{\"error\": \"Invalid timeout arg.  Must be 1-%d.\"}", maxTimeoutSeconds))
+			return
+		}
+		category := r.URL.Query().Get("category")
+		if len(category) == 0 || len(category) > 1024 {
+			if loggingEnabled {
+				log.Printf("Error: Invalid subscription category, must be 1-1024 characters long.\n")
+			}
+			io.WriteString(w, "{\"error\": \"Invalid subscription category, must be 1-1024 characters long.\"}")
+			return
+		}
+		// Default to only looking for current events
+		lastEventTime := time.Now()
+		// since_time is string of milliseconds since epoch
+		lastEventTimeParam := r.URL.Query().Get("since_time")
+		if len(lastEventTimeParam) > 0 {
+			// Client is requesting any event from given timestamp
+			// parse time
+			var parseError error
+			lastEventTime, parseError = millisecondStringToTime(lastEventTimeParam)
+			if parseError != nil {
+				if loggingEnabled {
+					log.Printf("Error parsing last_event_time arg. Parm Value: %s, Error: %s.\n",
+						lastEventTimeParam, err)
+				}
+				io.WriteString(w, "{\"error\": \"Invalid last_event_time arg.\"}")
+				return
+			}
+		}
+		subscription, err := newclientSubscription(category, lastEventTime)
+		if err != nil {
+			if loggingEnabled {
+				log.Printf("Error creating new Subscription: %s.\n", err)
+			}
+			io.WriteString(w, "{\"error\": \"Error creating new Subscription.\"}")
+			return
+		}
+		subscriptionRequests <- *subscription
+		// Listens for connection close and un-register subscription in the
+		// event that a client crashes or the connection goes down.  We don't
+		// need to wait around to fulfill a subscription if no one is going to
+		// receive it
+		disconnectNotify := w.(http.CloseNotifier).CloseNotify()
+		select {
+		case <-time.After(time.Duration(timeout) * time.Second):
+			// Lets the subscription manager know it can discard this request's
+			// channel.
+			clientTimeouts <- subscription.clientCategoryPair
+			timeout_resp := makeTimeoutResponse(time.Now())
+			if jsonData, err := json.Marshal(timeout_resp); err == nil {
+				io.WriteString(w, string(jsonData))
+			} else {
+				io.WriteString(w, "{\"error\": \"json marshaller failed\"}")
+			}
+		case events := <-subscription.Events:
+			// Consume event.  Subscription manager will automatically discard
+			// this client's channel upon sending event
+			// NOTE: event is actually []Event
+			if jsonData, err := json.Marshal(eventResponse{&events}); err == nil {
+				io.WriteString(w, string(jsonData))
+			} else {
+				io.WriteString(w, "{\"error\": \"json marshaller failed\"}")
+			}
+		case <-disconnectNotify:
+			// Client connection closed before any events occurred and before
+			// the timeout was exceeded.  Tell manager to forget about this
+			// client.
+			clientTimeouts <- subscription.clientCategoryPair
+		}
+	}
+}
