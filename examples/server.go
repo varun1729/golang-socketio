e28af42041bcfdd1d81ba6ffba48c67c5c1a1249
DESKTOP-EUBERBU\eddyf <eddyf@mail.com>
2017-10-13 04:03:27 +0300

    Advancing example

--- examples/server.go
+++ examples/server.go
@@ -1,40 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
-	"github.com/mtfelian/golang-socketio"
-	"github.com/mtfelian/golang-socketio/transport"
+	"github.com/geneva-lake/golang-socketio"
+	"github.com/geneva-lake/golang-socketio/transport"
 )
 
-type Channel struct {
+type ChannelInner struct {
 	Channel string `json:"channel"`
 }
 
-type Message struct {
+type MessageInner struct {
 	Id      int    `json:"id"`
 	Channel string `json:"channel"`
 	Text    string `json:"text"`
 }
 
+var currentRoot string
+var assetsDir http.FileSystem
+
 func main() {
-	server := gosocketio.NewServer(transport.GetDefaultWebsocketTransport())
+	currentRoot, _ := os.Getwd()
+	assetsDir = http.Dir(currentRoot + "\\assets")
+	fmt.Println(assetsDir)
+
+	server := gosocketio.NewServer(transport.GetDefaultPollingTransport())
 
 	server.On(gosocketio.OnConnection, func(c *gosocketio.Channel) {
 		log.Println("Connected")
 
-		c.Emit("/message", Message{10, "main", "using emit"})
+		//c.Emit("/message", MessageInner{10, "main", "using emit"})
 
-		c.Join("test")
-		c.BroadcastTo("test", "/message", Message{10, "main", "using broadcast"})
+		//c.Join("test")
+		//c.BroadcastTo("test", "/message", MessageInner{10, "main", "using broadcast"})
 	})
 	server.On(gosocketio.OnDisconnection, func(c *gosocketio.Channel) {
 		log.Println("Disconnected")
 	})
 
-	server.On("/join", func(c *gosocketio.Channel, channel Channel) string {
+	server.On("/join", func(c *gosocketio.Channel, channel ChannelInner) string {
 		time.Sleep(2 * time.Second)
 		log.Println("Client joined to ", channel.Channel)
 		return "joined to " + channel.Channel
@@ -43,6 +52,28 @@ func main() {
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/socket.io/", server)
 
+	//fs := http.FileServer(http.Dir("assets"))
+	//fmt.Println(fs)
+	serveMux.HandleFunc("/", AssetsFileHandler)
+
 	log.Println("Starting server...")
 	log.Panic(http.ListenAndServe(":3811", serveMux))
+}
+
+func AssetsFileHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" && r.Method != "HEAD" {
+		return
+	}
+	var file string = r.URL.Path
+	f, err := assetsDir.Open(file)
+	if err != nil {
+		log.Println("can not open file ", file, " ", err)
+		return
+	}
+	defer f.Close()
+	fi, err := f.Stat()
+	if err != nil {
+		log.Fatalln("can not open file ", file, " ", err)
+	}
+	http.ServeContent(w, r, file, fi.ModTime(), f)
 }
