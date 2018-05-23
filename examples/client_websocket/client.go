399cef25193182d7306e703780f358bf829ee2c2
Artemij Shepelev <ffelian@gmail.com>
2018-05-24 01:41:05 +0300

    rewritten go examples

---
+++ examples/client_websocket/client.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/mtfelian/golang-socketio"
+	"github.com/mtfelian/golang-socketio/examples/model"
+	"github.com/mtfelian/golang-socketio/transport"
+)
+
+const (
+	serverEventJoin  = "join"
+	serverEventSend  = "send"
+	serverEventLeave = "leave"
+
+	someEventName = "someEvent"
+)
+
+const defaultTimeout = time.Second * 30
+
+func onConnectionHandler(c *gosocketio.Channel)    { log.Printf("Connected %s\n", c.Id()) }
+func onDisconnectionHandler(c *gosocketio.Channel) { log.Printf("Disconnected %s\n", c.Id()) }
+
+// SomeEventPayload represents a payload for event someEvent
+type SomeEventPayload struct {
+	IntField    int    `json:"i"`
+	StringField string `json:"s"`
+}
+
+var sendResultC = make(chan SomeEventPayload)
+
+func onSomeEventHandler(c *gosocketio.Channel, data interface{}) {
+	log.Printf("--- Client channel %s received someEvent with data: %v\n", c.Id(), data)
+	j, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var obj SomeEventPayload
+	if err := json.Unmarshal(j, &obj); err != nil {
+		log.Fatal(err)
+	}
+
+	sendResultC <- obj
+	log.Printf("--- callback send data to channel")
+}
+
+func main() {
+	client, err := gosocketio.Dial(
+		gosocketio.AddrWebsocket("localhost", 3811, false),
+		transport.DefaultWebsocketTransport(),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := client.On(gosocketio.OnConnection, onConnectionHandler); err != nil {
+		log.Fatal(err)
+	}
+	if err := client.On(gosocketio.OnDisconnection, onDisconnectionHandler); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := client.On(someEventName, onSomeEventHandler); err != nil {
+		log.Fatal(err)
+	}
+
+	const roomName = "room1"
+
+	// join the room
+	joinResult, err := client.Ack(serverEventJoin, roomName, defaultTimeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("join result:", joinResult)
+
+	// make server to send back payload with Emit()
+	log.Println("awaiting payload back via server's Emit()...")
+	payloadToSend := model.Data{
+		EventName: someEventName,
+		Payload:   SomeEventPayload{7, "seven"},
+	}
+	sendResult, err := client.Ack(serverEventSend, payloadToSend, defaultTimeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("send result:", sendResult)
+
+	resultingPayload := <-sendResultC
+	log.Println("sent payload:", payloadToSend.Payload)
+	log.Println("resulting emitted payload:", resultingPayload)
+
+	// make server to send back payload with BroadcastTo()
+	log.Println("awaiting payload back via server's BroadcastTo()...")
+	payloadToSend = model.Data{
+		EventName:         someEventName,
+		BroadcastRoomName: roomName,
+		Payload:           SomeEventPayload{7, "seven"},
+	}
+	sendResult, err = client.Ack(serverEventSend, payloadToSend, defaultTimeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("send result:", sendResult)
+
+	resultingPayload = <-sendResultC
+	log.Println("resulting broadcasted payload:", resultingPayload)
+
+	leaveResult, err := client.Ack(serverEventLeave, roomName, defaultTimeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("leave result:", leaveResult)
+
+	client.Close()
+	log.Println("Client closed")
+}
---
+++ examples/server/server.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/mtfelian/golang-socketio"
+	"github.com/mtfelian/golang-socketio/examples/model"
+)
+
+var assetsDir http.FileSystem
+
+const port = 3811
+
+const OK = "OK"
+
+const (
+	eventJoin  = "join"
+	eventSend  = "send"
+	eventLeave = "leave"
+)
+
+func onConnectionHandler(c *gosocketio.Channel)    { log.Printf("Connected %s\n", c.Id()) }
+func onDisconnectionHandler(c *gosocketio.Channel) { log.Printf("Disconnected %s\n", c.Id()) }
+func onJoinHandler(c *gosocketio.Channel, roomName string) string {
+	log.Printf("Join %s to room %s\n", c.Id(), roomName)
+	if err := c.Join(roomName); err != nil {
+		return err.Error()
+	}
+	return OK
+}
+func onSendHandler(c *gosocketio.Channel, param interface{}) interface{} {
+	log.Printf("Received SEND on %s with ", c.Id())
+	j, err := json.Marshal(param)
+	if err != nil {
+		log.Println("error:", err)
+		return err
+	}
+	log.Printf("payload JSON is %s\n", j)
+
+	var obj model.Data
+	if err := json.Unmarshal(j, &obj); err != nil {
+		log.Println("error:", err)
+		return err
+	}
+
+	if obj.BroadcastRoomName == "" {
+		if err := c.Emit(obj.EventName, obj.Payload); err != nil {
+			return err
+		}
+		return OK
+	}
+
+	c.BroadcastTo(obj.BroadcastRoomName, obj.EventName, obj.Payload)
+	return OK
+}
+func onLeaveHandler(c *gosocketio.Channel, roomName string) string {
+	log.Printf("Leave %s from room %s\n", c.Id(), roomName)
+	if err := c.Leave(roomName); err != nil {
+		return err.Error()
+	}
+	return OK
+}
+
+func main() {
+	currentRoot, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	d := filepath.Join(currentRoot, "..", "assets")
+	if !fileExists(d) {
+		d = filepath.Join(currentRoot, "examples", "assets")
+	}
+
+	assetsDir = http.Dir(d)
+
+	log.Println("assetsDir:", assetsDir)
+
+	server := gosocketio.NewServer()
+	if err := server.On(gosocketio.OnConnection, onConnectionHandler); err != nil {
+		log.Fatal(err)
+	}
+	if err := server.On(gosocketio.OnDisconnection, onDisconnectionHandler); err != nil {
+		log.Fatal(err)
+	}
+	if err := server.On(eventJoin, onJoinHandler); err != nil {
+		log.Fatal(err)
+	}
+	if err := server.On(eventSend, onSendHandler); err != nil {
+		log.Fatal(err)
+	}
+	if err := server.On(eventLeave, onLeaveHandler); err != nil {
+		log.Fatal(err)
+	}
+
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/socket.io/", server)
+	serveMux.HandleFunc("/", assetsFileHandler)
+
+	log.Println("Starting server...")
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), serveMux); err != nil {
+		log.Panic(err)
+	}
+}
+
+// fileExists returns true if a file with given path exists
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
+}
+
+// assetsFileHandler for static Data
+func assetsFileHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet && r.Method != http.MethodHead {
+		return
+	}
+
+	file := r.URL.Path
+	f, err := assetsDir.Open(file)
+	if err != nil {
+		log.Fatalf("can't open file %s: %v\n", file, err)
+		return
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		log.Fatalf("can't open file %s: %v\n", file, err)
+		return
+	}
+
+	http.ServeContent(w, r, file, fi.ModTime(), f)
+}
